d8a27d758ca577b307e24331419a19e3e0834840
rel7z <itaaar272@gmail.com>
2026-09-20 19:22:30 +0700

    go

--- components/ui/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-interface SkeletonProps {
-  className?: string;
-}
-
-export function Skeleton({ className = "" }: SkeletonProps) {
-  return (
-    <div className={`animate-pulse rounded bg-gray-800 ${className}`} />
-  );
-}
